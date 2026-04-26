7215e5bb1764d83ebd008ebebd0d2e45b9856a45
JithanRoy <jithan.roy@tecnonext.com>
2026-04-26 10:10:11 +0600

    feat: add animated fire glow border overlay to application layout

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,8 @@ import { About, Contact, Header, Home, ParticlesContainer, Projects, Skills, Foo
 const App = () => {
   return (
     <div className="w-full xl:w-[1600px] py-12 md:py-32 px-4 md:px-12 lg:px-32 mx-auto overflow-hidden">
+        {/* Fire Glow Border */}
+        <div className="fixed inset-0 border-2 border-transparent pointer-events-none z-[9999] animate-fire"></div>
         {/* particles container */}  
         <ParticlesContainer />
         {/* header */}
